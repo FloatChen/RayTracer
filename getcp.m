5cf673f5206fe4b8d0a1b86a7cd9b38eceeafff8
sccrosby <sccrosby@ucsd.edu>
2017-02-05 12:52:13 -0800

    new version

--- getcp.m
+++
@@ -1,9 +0,0 @@
-function cp = getcp(f,h) 
-% [cp, cg] = getc(f,h) 
-%g = 9.81;
-k = getk(f,h);
-%cg = 0.5*(g*tanh(k*h)+g*(k*h)*(sech(k*h)^2))/sqrt(g*k*tanh(k*h));
-%cp = sqrt(g*k*tanh(k*h))/k;
-cp = 2*pi*f/k;
-
-end
